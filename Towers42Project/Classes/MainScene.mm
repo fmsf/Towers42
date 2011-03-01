890e7facd1f4c222c4c344a557b18752b95da3a5
Francisco Ferreira <francisco.m.s.ferreira@gmail.com>
2011-03-01 15:34:27 +0000

    Added map drawing

--- Towers42Project/Classes/MainScene.mm
+++ Towers42Project/Classes/MainScene.mm
@@ -43,10 +43,20 @@
 		// position the label on the center of the screen
 		label.position =  ccp( size.width /2 , size.height/2 );
 		
+		
+		
+		myGui = [[GameGUI alloc] init];
+		for (CCSprite* sprite in [myGui getElements]){
+			[self addChild:sprite];
+		}
+			
 		// add the label as a child to this Layer
-		[self addChild: label];
+		//[self addChild: label];
 		controller = [[Controller alloc] init];
 		[self schedule:@selector(tick:)];
+		
+		
+		
 
 	}
 	return self;
@@ -56,7 +66,26 @@
 /*	int t=123;
 	NSLog(@"Um exemplo de log %d", t);*/
 	[controller execute];
+//	NSMutableArray *
 }
+
+// You have to over-ride this method
+-(void)draw{
+    glColor4f(0.8, 1.0, 0.76, 1.0);  
+    glLineWidth(2.0f);
+	//ccDrawLine(ccp(300,470), ccp(100,100));
+//    drawLine(10,100,50,79);
+	unsigned int i;
+	NSMutableArray* path = [controller getMapPath];
+	for(i=1;i<[path count];i++){ // TODO: FIXXX
+		NSValue *val = [path objectAtIndex:i-1]; 
+		CGPoint p1 = [val CGPointValue];
+		val = [path objectAtIndex:i];
+		CGPoint p2 = [val CGPointValue];
+		ccDrawLine(p1,p2);
+	}
+
+} 
 
 // on "dealloc" you need to release all your retained objects
 - (void) dealloc
